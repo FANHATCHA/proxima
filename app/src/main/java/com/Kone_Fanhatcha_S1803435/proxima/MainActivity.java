218712ed8ba9f31a1ebd7cf2575652e155f6f0ba
Kone Fanhatcha <fanhatcha@projanalytics.com>
2020-02-12 11:12:45 +0400

    Inital commit

---
+++ app/src/main/java/com/Kone_Fanhatcha_S1803435/proxima/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Kone_Fanhatcha_S1803435.proxima;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
